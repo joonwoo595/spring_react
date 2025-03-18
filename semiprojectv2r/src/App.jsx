666cb9a37ddfb3c8fad3d0721cefc73601fde1ae
joonwoo595 <jonbeo312@gmail.com>
2025-03-18 12:16:00 +0900

    Join, NotFound 출력

--- semiprojectv2r/src/App.jsx
+++ semiprojectv2r/src/App.jsx
@@ -1,24 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 import React from "react";
-import Header from "./pages/layout/Header.jsx";
-import Nav from "./pages/layout/Nav.jsx"
-import Footer from "./pages/layout/Footer.jsx";
-import Main from "./pages/Main.jsx";
-import './styles/App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./pages/layout/Header";
+import Nav from "./pages/layout/Nav";
+import Footer from "./pages/layout/Footer";
+import Main from "./pages/Main";
+import Join from "./pages/Join";
+import NotFound from "./pages/NotFound";
+import './styles/App.css';
 
 function App() {
-
-
     return (
-        <>
+        <Router>
             <Header/>
             <Nav/>
-            <Main/>
+            <Routes>
+                <Route path="/" element={<Main/>}/>
+                <Route path="/member/join" element={<Join/>}/>
+                {/*<Route path="/member/login" element={<Login/>}/>*/}
+                {/*<Route path="/board/list" element={<BoardList/>}/>*/}
+                {/*<Route path="/gallery/list" element={<GalleryList/>}/>*/}
+                {/*<Route path="/member/myinfo" element={<Myinfo/>}/>*/}
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
             <Footer/>
-        </>
-    )
+        </Router>
+    );
 }
 
-export default App
+export default App;
