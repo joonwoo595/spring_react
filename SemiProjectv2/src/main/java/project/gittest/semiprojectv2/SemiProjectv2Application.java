a8f6e5cc273b2bcba37561872e1896435c9c7838
joonwoo595 <jonbeo312@gmail.com>
2025-03-18 10:12:00 +0900

    Initial commit

---
+++ SemiProjectv2/src/main/java/project/gittest/semiprojectv2/SemiProjectv2Application.java
@@ -0,0 +1,13 @@
+package project.gittest.semiprojectv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiProjectv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemiProjectv2Application.class, args);
+	}
+
+}
---
+++ SemiProjectv2/src/test/java/project/gittest/semiprojectv2/SemiProjectv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package project.gittest.semiprojectv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiProjectv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
