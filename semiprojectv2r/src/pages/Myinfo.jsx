c28b78136bc3ac5838cc0ed2cd05f9d9aaa36e98
joonwoo595 <jonbeo312@gmail.com>
2025-03-18 12:44:21 +0900

    myinfo 출력

--- semiprojectv2r/src/pages/Myinfo.jsx
+++ semiprojectv2r/src/pages/Myinfo.jsx
@@ -6,33 +6,32 @@ const Myinfo = () =>{
     return (
         <main id="content">
             <h2>회원정보</h2>
-
             <div id="myinfo">
                 <table className="table table-bordered mb-0">
 
                     <colgroup>
-                        <col style="width:30%"/>
+                        <col style={{width:"30%"}}/>
                         <col/>
                     </colgroup>
                     <tbody>
                     <tr>
                         <td>아이디</td>
-
+                        <td><span>abc123</span></td>
                     </tr>
 
                     <tr>
                         <td>이름</td>
-
+                        <td><span >abc123</span></td>
                     </tr>
 
                     <tr>
                         <td>이메일</td>
-
+                        <td><span>abc123</span></td>
                     </tr>
 
                     <tr>
                         <td>가입일</td>
-
+                        <td><span>abc123</span></td>
                     </tr>
                     </tbody>
 
