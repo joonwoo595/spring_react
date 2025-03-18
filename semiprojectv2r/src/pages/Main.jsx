c255258eac2fced4e5b3b4887a7ea4e6a8900739
joonwoo595 <jonbeo312@gmail.com>
2025-03-18 11:17:09 +0900

    Nav/Footer 출력

---
+++ semiprojectv2r/src/pages/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+// Header 함수 컴포넌트 정의
+const Main = () =>{
+    return (
+        <main id="content">
+            <img src="image/img.jpg" className="img-fluid"/>
+        </main>
+    )
+ }
+
+ export default Main;
---
+++ semiprojectv2r/src/pages/layout/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Header 함수 컴포넌트 정의
+const Fotter = () =>{
+    return (
+        <footer className="navbar-nav navbar-light bg-light">
+            <span className="text-center py-2">Copyright &copy; 2024. yjw. All rights reserved.</span>
+        </footer>
+    )
+}
+
+export default Fotter;
