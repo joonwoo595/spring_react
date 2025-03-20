f0013103c18ba31aebe75b59252c49766fb6bae2
joonwoo595 <jonbeo312@gmail.com>
2025-03-20 15:17:56 +0900

    BoardList, BoardWrite 출력

--- semiprojectv2r/src/App.jsx
+++ semiprojectv2r/src/App.jsx
@@ -8,8 +8,10 @@ import Join from "./pages/Join";
 import Login from "./pages/Login";
 import Myinfo from "./pages/Myinfo";
 import BoardList from "./pages/BoardList";
+import BoardWrite from "./pages/BoardWrite.jsx";
 import NotFound from "./pages/NotFound";
 import './styles/App.css'
+
 
 function App() {
 
@@ -22,6 +24,7 @@ function App() {
         <Route path="/member/join" element={<Join />} />
         <Route path="/member/login" element={<Login />} />
         <Route path="/board/list" element={<BoardList />} />
+        <Route path="/board/write" element={<BoardWrite />} />
         {/*<Route path="/gallery/list" element={<GalleryList />} />*/}
         <Route path="/member/myinfo" element={<Myinfo />} />
         <Route path="*" element={<NotFound />} />
